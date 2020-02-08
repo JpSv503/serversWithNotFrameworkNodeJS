c7f37970e7757cb2668e5acf91351a016c640fec
Javier <javibautista318@gmail.com>
2020-02-07 22:42:04 -0600

    modulo de servers agregado

---
+++ server.js
@@ -0,0 +1,22 @@
+const HTTP = require("http");
+
+//handleServer->manejador del servidor
+
+const handleServer = function(req, res){
+    //mostramos tipo de contenido, tipo de respuesta
+    res.writeHead(200, { 'content-type': 'text/html'}); //Indicamos un estado de respuesta
+    res.write('<h1>Hola mundo</h1>');
+    res.end();
+ }
+
+ const server = HTTP.createServer(handleServer);
+ server.listn(3000, function(){
+     console.log('server on port 3000');
+ });
+
+
+
+ /**
+ *NPM y frameworks ============ lo habla en 1hh17mm;
+ * npmjs.com
+ */
